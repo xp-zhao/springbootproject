46d1bc48a4da0606b8de4d777cb289ded3446d79
xp-zhao <13688396271@163.com>
2018-12-17 17:53:56 +0800

    '开始整合zookeeper'

---
+++ src/main/java/com/example/springbootproject/zk/autoconfig/ZooKeeperAutoConfig.java
@@ -0,0 +1,50 @@
+package com.example.springbootproject.zk.autoconfig;
+
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xp-zhao on 2018/12/17.
+ */
+@Configuration
+@EnableConfigurationProperties(ZooKeeperConfig.class)
+public class ZooKeeperAutoConfig
+{
+	private static final Logger logger = LoggerFactory.getLogger(ZooKeeperAutoConfig.class);
+
+	@Bean
+	@ConditionalOnMissingBean(ZooKeeper.class)
+	public ZooKeeper init(ZooKeeperConfig config){
+		ZooKeeper result = null;
+		CountDownLatch connectedSemaphore = new CountDownLatch(1);
+		try
+		{
+			result = new ZooKeeper(config.getHost() , config.getConnectionWaitTime() , event -> {
+				if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+					connectedSemaphore.countDown();
+				}
+			});
+			//如果正常结束,就返回zk连接,否则异常退出
+			if(connectedSemaphore.await(config.getConnectionWaitTime(), TimeUnit.SECONDS))
+			{
+				return result;
+			}
+		}
+		catch (Exception e)
+		{
+			logger.error("连接 zk 失败");
+		}
+
+		return result;
+	}
+}
---
+++ src/main/java/com/example/springbootproject/zk/autoconfig/ZooKeeperConfig.java
@@ -0,0 +1,25 @@
+package com.example.springbootproject.zk.autoconfig;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by xp-zhao on 2018/12/17.
+ */
+@Data
+@ConfigurationProperties (prefix = "spring.zookeeper")
+public class ZooKeeperConfig
+{
+	/**
+	 * zk连接地址
+	 */
+	private String host;
+	/**
+	 * zk连接过期时间
+	 */
+	private int sessiontimetou;
+	/**
+	 * 连接zookeeper时的超时时长,单位为秒，默认为30秒
+	 */
+	private int connectionWaitTime=30;
+}
