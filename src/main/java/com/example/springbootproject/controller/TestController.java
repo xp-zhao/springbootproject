e2f6c74063ff45c01c784d3cfd9f3760621277a7
xp-zhao <13688396271@163.com>
2019-02-27 17:59:18 +0800

    '整合kafka-未完'

--- src/main/java/com/example/springbootproject/controller/TestController.java
+++ src/main/java/com/example/springbootproject/controller/TestController.java
@@ -6,6 +6,8 @@ import com.example.springbootproject.model.resp.ReturnInfo;
 import com.example.springbootproject.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,8 @@ public class TestController
 	public BaseResponse test(@Valid BaseRequest req)
 	{
 		System.out.println(service.get("2013051233","xp"));
+		logger.info( MarkerFactory.getMarker("monitor"),"test-log-info");
+		logger.debug( MarkerFactory.getMarker("monitor"),"test-log-debug");
 		return new BaseResponse(ReturnInfo.SUCCESS);
 	}
 	@RequestMapping("/log")
