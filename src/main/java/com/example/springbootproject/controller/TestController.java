d2c9ca9ccb80e662b47b35bb041f4e871d842c2a
zhaoxp <zhaoxp@youxuepai.com>
2019-05-28 18:00:33 +0800

    接收list参数测试

--- src/main/java/com/example/springbootproject/controller/TestController.java
+++ src/main/java/com/example/springbootproject/controller/TestController.java
@@ -4,38 +4,45 @@ import com.example.springbootproject.model.req.BaseRequest;
 import com.example.springbootproject.model.resp.BaseResponse;
 import com.example.springbootproject.model.resp.ReturnInfo;
 import com.example.springbootproject.service.TestService;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 /**
  * Created by xp-zhao on 2018/11/15.
  */
 @RestController
-public class TestController
-{
-	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+public class TestController {
 
-	@Autowired
-	private TestService service;
+  private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
-	@RequestMapping("/test")
-	public BaseResponse test(@Valid BaseRequest req)
-	{
-		System.out.println(service.get("2013051233","xp"));
-		logger.info("test-log-info");
-		logger.debug("test-log-debug");
-		return new BaseResponse(ReturnInfo.SUCCESS);
-	}
-	@RequestMapping("/log")
-	public BaseResponse log(@Valid BaseRequest req)
-	{
-		return new BaseResponse(ReturnInfo.SUCCESS);
-	}
+  @Autowired
+  private TestService service;
+
+  @RequestMapping("/test")
+  public BaseResponse test(@Valid BaseRequest req,
+      @NotNull(message = "不能为空") @RequestHeader("test-header") String testHeader) {
+    System.out.println(service.get("2013051233", "xp"));
+    logger.info("test-log-info");
+    logger.debug("test-log-debug");
+    return new BaseResponse(ReturnInfo.SUCCESS);
+  }
+
+  @RequestMapping("/log")
+  public BaseResponse log(@Valid BaseRequest req) {
+    return new BaseResponse(ReturnInfo.SUCCESS);
+  }
+
+  @RequestMapping("/list")
+  public BaseResponse list(@RequestBody List<Integer> list) {
+    System.out.println(list);
+    return new BaseResponse(ReturnInfo.SUCCESS);
+  }
 }
